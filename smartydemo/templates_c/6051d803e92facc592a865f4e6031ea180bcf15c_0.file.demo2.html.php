9d8bcc6c8f8e06781b344acc3bacb66a83092306
admin <penghua.wang@condenast.com.cn>
2018-06-13 14:56:38 +0800

    smarty中 for while 循环的使用

--- smartydemo/templates_c/6051d803e92facc592a865f4e6031ea180bcf15c_0.file.demo2.html.php
+++ smartydemo/templates_c/6051d803e92facc592a865f4e6031ea180bcf15c_0.file.demo2.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32, created on 2018-06-13 14:49:28
+/* Smarty version 3.1.32, created on 2018-06-13 14:56:06
   from '/Users/admin/Sites/php_project/smartydemo/templates/demo2.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_5b20be78221db6_83000524',
+  'unifunc' => 'content_5b20c006a40e57_75116962',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '6051d803e92facc592a865f4e6031ea180bcf15c' => 
     array (
       0 => '/Users/admin/Sites/php_project/smartydemo/templates/demo2.html',
-      1 => 1528872566,
+      1 => 1528872964,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5b20be78221db6_83000524 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5b20c006a40e57_75116962 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -116,6 +116,8 @@ $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
 		差<br>
 	<?php }?>
 
+	<!-- section循环 -->
+<!--  
 	<?php
 $__section_one_0_loop = (is_array(@$_loop=$_smarty_tpl->tpl_vars['onearray']->value) ? count($_loop) : max(0, (int) $_loop));
 $_smarty_tpl->tpl_vars['__smarty_section_one'] = new Smarty_Variable(array('total' => $__section_one_0_loop));
@@ -126,7 +128,7 @@ $_smarty_tpl->tpl_vars['__smarty_section_one']->value['index_next'] = $_smarty_t
 $_smarty_tpl->tpl_vars['__smarty_section_one']->value['first'] = ($_smarty_tpl->tpl_vars['__smarty_section_one']->value['iteration'] === 1);
 $_smarty_tpl->tpl_vars['__smarty_section_one']->value['last'] = ($_smarty_tpl->tpl_vars['__smarty_section_one']->value['iteration'] === $_smarty_tpl->tpl_vars['__smarty_section_one']->value['total']);
 ?>  
-<!--  	index=<?php echo (isset($_smarty_tpl->tpl_vars['__smarty_section_one']->value['index']) ? $_smarty_tpl->tpl_vars['__smarty_section_one']->value['index'] : null);?>
+ 	index=<?php echo (isset($_smarty_tpl->tpl_vars['__smarty_section_one']->value['index']) ? $_smarty_tpl->tpl_vars['__smarty_section_one']->value['index'] : null);?>
 ,  
  	index_prev=<?php echo (isset($_smarty_tpl->tpl_vars['__smarty_section_one']->value['index_prev']) ? $_smarty_tpl->tpl_vars['__smarty_section_one']->value['index_prev'] : null);?>
 ,  
@@ -137,15 +139,35 @@ $_smarty_tpl->tpl_vars['__smarty_section_one']->value['last'] = ($_smarty_tpl->t
  	last=<?php echo (isset($_smarty_tpl->tpl_vars['__smarty_section_one']->value['last']) ? $_smarty_tpl->tpl_vars['__smarty_section_one']->value['last'] : null);?>
 ,  
  	iteration =<?php echo (isset($_smarty_tpl->tpl_vars['__smarty_section_one']->value['iteration']) ? $_smarty_tpl->tpl_vars['__smarty_section_one']->value['iteration'] : null);?>
-,  --> 
- 	<!-- total=<?php echo (isset($_smarty_tpl->tpl_vars['__smarty_section_one']->value['total']) ? $_smarty_tpl->tpl_vars['__smarty_section_one']->value['total'] : null);?>
-,   -->
+,  
+ 	total=<?php echo (isset($_smarty_tpl->tpl_vars['__smarty_section_one']->value['total']) ? $_smarty_tpl->tpl_vars['__smarty_section_one']->value['total'] : null);?>
+,  
  	value=<?php echo $_smarty_tpl->tpl_vars['onearray']->value[(isset($_smarty_tpl->tpl_vars['__smarty_section_one']->value['index']) ? $_smarty_tpl->tpl_vars['__smarty_section_one']->value['index'] : null)];?>
 <br />  
 	<?php
 }
 }
-?> 
+?>  -->
+
+	<!-- for循环 -->
+	<?php
+$_smarty_tpl->tpl_vars['i'] = new Smarty_Variable(null, $_smarty_tpl->isRenderingCache);$_smarty_tpl->tpl_vars['i']->step = 1;$_smarty_tpl->tpl_vars['i']->total = (int) ceil(($_smarty_tpl->tpl_vars['i']->step > 0 ? 10+1 - (1) : 1-(10)+1)/abs($_smarty_tpl->tpl_vars['i']->step));
+if ($_smarty_tpl->tpl_vars['i']->total > 0) {
+for ($_smarty_tpl->tpl_vars['i']->value = 1, $_smarty_tpl->tpl_vars['i']->iteration = 1;$_smarty_tpl->tpl_vars['i']->iteration <= $_smarty_tpl->tpl_vars['i']->total;$_smarty_tpl->tpl_vars['i']->value += $_smarty_tpl->tpl_vars['i']->step, $_smarty_tpl->tpl_vars['i']->iteration++) {
+$_smarty_tpl->tpl_vars['i']->first = $_smarty_tpl->tpl_vars['i']->iteration === 1;$_smarty_tpl->tpl_vars['i']->last = $_smarty_tpl->tpl_vars['i']->iteration === $_smarty_tpl->tpl_vars['i']->total;?>
+		<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
+ :11111 <br>
+	<?php }
+}
+?>
+
+	<!-- while循环 -->
+	<?php $_smarty_tpl->_assignInScope('i', 1);?>
+	<?php
+ while (10 > $_smarty_tpl->tpl_vars['i']->value) {?>
+		<?php echo $_smarty_tpl->tpl_vars['i']->value++;?>
+:22222 <br>
+	<?php }?>
 
 </body>
 </html><?php }
